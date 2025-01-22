7cb5426099e034b678a5625f36cb7e503f84eff6
sirosh <armanakhov@ya.ru>
2025-01-22 20:38:47 +0300

    change logs

--- plugins/shikimori_sync/listenets.js
+++ plugins/shikimori_sync/listenets.js
@@ -5,7 +5,6 @@ function init() {
 
     async function sendUserStats() {
         let shikimori_id = await currently_playing.shikimori_id;
-        console.log("found shikimori id ", shikimori_id);
         await Utils.setUserRate(currently_playing.episode,
             currently_playing.episode === currently_playing.total_episodes ? "complete" : "watching",
             shikimori_id);
@@ -15,6 +14,7 @@ function init() {
         currently_playing.progress = (e.current / e.duration * 100);
     });
     Lampa.Favorite.listener.follow('add,added', function (e) {
+        console.log("shikimori_sync: added event ", e);
         if (e.where === 'history' && e.card.keywords.results.some(kw => kw.name = 'anime')) {
             console.log(e.card);
             currently_playing.suitable = true;
@@ -27,13 +27,15 @@ function init() {
         }
     });
     Lampa.Player.listener.follow('start', async function (e) {
+        console.log("shikimori_sync: player start ", e);
         if (currently_playing.suitable) {
             currently_playing.season = e.season;
             currently_playing.episode = e.episode - 1;
             await sendUserStats();
         }
     });
-    Lampa.Player.listener.follow('destroy', async function () {
+    Lampa.Player.listener.follow('destroy', async function (e) {
+        console.log("shikimori_sync: player destroy ", e);
         if (currently_playing.suitable && currently_playing.progress > 85) {
             currently_playing.episode += 1;
             await sendUserStats();
